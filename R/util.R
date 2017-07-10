46b0408c102c5e85624f519ef3fbbbbae0e4c84f
Danny McClanahan <danieldmcclanahan@gmail.com>
2017-07-09 21:39:25 -0500

    upd8

--- R/util.R
+++ R/util.R
@@ -4,21 +4,47 @@
 
 
 
+### Libraries loaded.
 library(magrittr)
 
 
 
 ### Type checking and assertions.
+assert <- function (expr, msg, expected = TRUE, test = identical) {
+    se <- substitute(expr)
+    res <- eval.parent(se)
+    if (!match.fun(test)(expected, res)) {
+        ## failed
+        if (!hasArg(msg)) {
+            ex_s <- deparse(expr)
+            msg <- sprintf(
+                paste("the expression '%s' evaluated to '%s',",
+                      "but '%s' was expected (with test '%s')"),
+                ex_s, res, expected, test)
+            stopifnot(isTRUE(is.character(msg)))
+        }
+        stop(msg)
+    }
+    res
+}
+
 closure_pure <- function (block) {
     bs <- substitute(block)
     eval.parent(bs)
 }
 
-int__convert_named_arglist <- function (x) {
+closure_one <- function (block, nm = ".") {
+    bs <- substitute(block)
+    argl <- .convert_named_arglist(list(nm))
+    ret <- list(as.pairlist(argl), bs)
+    eval.parent(do.call('function', ret))
+}
+
+.convert_named_arglist <- function (x) {
     n <- length(x)
-    if (0 == n) { return(list()) }
     alt <- as.list(x)
     nms <- names(alt)
+    if (is.null(nms)) { nms <- rep('', n) }
     name_only <- (nms == '')
     objs <- rep(alist(. =), n)
     objs[!name_only] <- alt[!name_only]
@@ -26,96 +52,110 @@ int__convert_named_arglist <- function (x) {
     setNames(objs, unlist(nms))
 }
 
-int__conf_list <- function (...) {
-    args <- as.list(substitute(list(...)))[-1L]
-    env <- parent.frame()
-    f <- bquote(function (arg) { eval(arg, .(env)) })
-    int__convert_named_arglist(args) %>% lapply(f)
+.convert_callable <- function (x,
+                               arglist = .convert_named_arglist(list(".")),
+                               env = parent.frame(),
+                               vectorize = TRUE) {
+    sx <- substitute(x)
+    conv <- if (is.language(sx)) {
+                b <- bquote(
+                    do.call('function', list(.(as.pairlist(arglist)), .(sx))))
+                eval(b, env)
+            } else if (is.function(x)) {
+                match.fun(x)
+            } else {
+                x
+            }
+    if (vectorize) { Vectorize(conv) } else { conv }
 }
 
-int__merge_env_strats <- list(
-    left = setdiff,
-    right = setdiff,
-    both = intersect,
-    either = union,
-    neither = function (x, y) {
-        ld <- setdiff(x, y)
-        rd <- setdiff(y, x)
-        intersect(ld, rd)
-    })
-
-merge.env <- function (base, new, strategy = int__merge_env_strats[1]) {
-    be <- as.environment(base)
-    switch()
-    new_env <- if (override) { new } else { base }
-    new_keys <- as.environment(new_env) %>% ls(all.names = T)
-}
-
-int__standard_macro_args <- list(
-    block,
-    env = parent.frame(),
-    .dots = as.list(substitute(list(...)))[-1L]
-)
-
-## int__class_macro_args <-
-
-make_class <- function (...) {
-    ## args <-
-    args <- list(name)
-    cl <- do.call('setClass', list(name))
-    setClass(name, slots = slots, )
-}
-
-setClass("Condition",
-         slots = list(pred = "function",
-                      arity = "logical",
-                      shortcircuit = "logical"))
-setClass("Filter",
-         slots = list(conditions = "list"),
-         validity = function (object) {
-             object@conditions %.% is("Condition") %>% unlist %>% all
-         })
-
-assert <- function (expr, msg) {
-    sub_cond <- sub(expr)
-    if (hasArg(msg)) {
-        bquote(if (.(expr)) {
-                   stop(.(msg))
-               })
+.update_env <- function (base, new, nms) {
+    gen <- new.env(parent = base)
+    for (nm in nms) {
+        assign(nm, get(nm, new), gen)
     }
-    subbed <- sub(expr)
-    cl <- bquote(stopifnot(isTRUE(.(expr))))
-    eval.parent(cl)
+    gen
 }
 
+`%&%` <- function (arglist, block) {
+    as <- arglist %>% .convert_named_arglist %>% as.pairlist
+    bs <- substitute(block)
+    eval.parent(do.call("function", list(as, bs)))
+}
+
+.conf_list <- function (.) {
+    argl <- .convert_named_arglist(list("x", "y")) %>% as.pairlist
+    .convert_callable(., arglist = argl)
+}
+
+.merge_env_strats <- .conf_list(
+    quote(list(
+        left = setdiff(y, x),
+        right = setdiff,
+        both = intersect,
+        either = union,
+        neither = function (x, y) {
+            ld <- setdiff(x, y)
+            rd <- setdiff(y, x)
+            intersect(ld, rd)
+        })))
+
+
+merge_env <- function (base, new, strategy = .merge_env_strats[[1]]) {
+    b_env <- as.environment(base)
+    n_env <- as.environment(new)
+    f <- match.fun(strategy) %>% .convert_callable
+    .update_env(b_env, n_env, f(names(b_env), names(n_env)))
+}
+
+.make_standard_macro <- function (block, env = parent.frame(),
+                                  arglist = .convert_named_arglist(list(".")),
+                                  ...) {
+    args <- as.list(substitute(list(...)))[-1L]
+    bs <- substitute(block)
+    print(bs)
+    .convert_callable(
+        bs, .convert_named_arglist(list(".", "env", "arglist", "...")))
+}
+
+## make_class <- function (...) {
+##     ## args <-
+##     args <- list(name)
+##     cl <- do.call('setClass', list(name))
+##     setClass(name, slots = slots, )
+## }
+
+## setClass("Condition",
+##          slots = list(pred = "function",
+##                       arity = "logical",
+##                       shortcircuit = "logical"))
+## setClass("Filter",
+##          slots = list(conditions = "list"),
+##          validity = function (object) {
+##              object@conditions %.% is("Condition") %>% unlist %>% all
+##          })
 
 
 ## ### anonymous functions (working with magrittr)
-int__closure_creator <- function (block, env = parent.frame()) {
-    sb <- substitute(block)
-    eval(sb, env)
-}
+## .closure_creator <- function (block, env = parent.frame()) {
+##     sb <- substitute(block)
+##     eval(sb, env)
+## }
 
-curry <- function (fn, n = 1) {
-    assert()
-}
+## curry <- function (fn, n = 1) {
+##     assert()
+## }
 
 ## 0-arg anonymous function
-## closure_pure <- int__closure_creator function (block) {
+## closure_pure <- .closure_creator function (block) {
 ##     env <- parent.frame()
 ##     bs <- substitute(block)
 ##     function () { eval(bs, env) }
 ## }
 
 ## 1-arg anonymous function with arg `.`
-closure_ <- function (block, env = parent.frame()) {
-    eval(bquote(function (.) { .(substitute(block)) }), env)
-}
-
-## `%&%` <- function (arglist, block) {
-##     as <- substitute(arglist) %>% int__convert_named_arglist %>% as.pairlist
-##     bs <- substitute(block)
-##     eval.parent(do.call("function", list(as, bs)))
+## closure_ <- function (block, env = parent.frame()) {
+##     eval(bquote(function (.) { .(substitute(block)) }), env)
 ## }
 
 ## ## mapper_fun: (x) -> x
@@ -166,7 +206,7 @@ closure_ <- function (block, env = parent.frame()) {
 ##     })
 ## }
 
-## match.language <- function (x) {
+## match.blockuage <- function (x) {
 ##     stopifnot(is.language(x))
 ##     x
 ## }
@@ -243,3 +283,7 @@ closure_ <- function (block, env = parent.frame()) {
 ## filter <- function (x, f) { Filter(f, x) }
 
 ## `%!%` <- function (a, b) { filter(a, b) }
+
+## Local Variables:
+## ess-r-package-info: ("acceleRated" . "/home/cosmicexplorer/tools/acceleRated/")
+## End:
